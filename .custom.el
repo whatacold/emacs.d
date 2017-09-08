f81388122f35031d69205fb5758dcb46bf423386
whatacold <whatacold@gmail.com>
2017-09-08 09:21:59 +0800

    a bit tuning about font-face, w3m, neotree etc

--- .custom.el
+++ .custom.el
@@ -50,15 +50,29 @@
       (when *unix* ; linux or unix
         (or (executable-find "chrome") "/opt/google/chrome/chrome")))
 
+(setq w3m-user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36")
+
 ;; maximized
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
+(setq neo-autorefresh nil)
+
 ;; Windows
 (if *win64*
   (progn
+    (set-face-attribute 'default nil :height 140)
     (when (file-directory-p "f:/cygwin64/bin")
       (add-to-list 'exec-path "f:/cygwin64/bin"))
     (setq shell-file-name "bash")
     (setq explicit-shell-file-name shell-file-name)
     (setq tramp-default-method "scp")))
+
+(defun my-rsync ()
+  "Util to easily rsync projects"
+  (interactive)
+  (shell-command (format "rsync.sh %s" (my-root-dir))))
+
+(eval-after-load "evil"
+  (nvmap :prefix ","
+         "sy" 'my-rsync))
