8f8732224f241c41353361c09d115285d590fb50
whatacold <whatacold@gmail.com>
2017-10-05 10:35:48 +0800

    define my-set-font-height

--- .custom.el
+++ .custom.el
@@ -79,3 +79,20 @@
 (eval-after-load "evil"
   (nvmap :prefix ","
          "sy" 'my-rsync))
+
+(defun my-set-font-height (height)
+  "set font height to HEIGHT"
+  (interactive "P")
+  (if (> height 24)
+      (message "font height too large: %d" height)
+    (let* ((new-height (* 10 height))
+           (new-point-height (/ new-height 10)))
+      (dolist (f (frame-list))
+        (with-selected-frame f
+          ;; Latest 'set-frame-font supports a "frames" arg, but
+          ;; we cater to Emacs 23 by looping instead.
+          (set-frame-font (font-name-replace-size (face-font 'default)
+                                                  new-point-height)
+                          t)))
+      (set-face-attribute 'default nil :height new-height)
+      (message "default font size is now %d" new-point-height))))
