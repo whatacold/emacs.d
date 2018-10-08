dda2bf3e4d15fc1fe5cd1480750652ba6e0eee89
whatacold <whatacold@gmail.com>
2018-10-08 21:50:48 +0800

    Use <up> and <down> for section navigation of magit status.

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -13,6 +13,12 @@
 (global-set-key (kbd "C-i") #'counsel-ag)
 (global-set-key (kbd "M-l") #'counsel-imenu)
 
+;; magit
+(eval-after-load 'magit
+  '(progn
+     (define-key magit-status-mode-map (kbd "<down>") #'magit-section-forward)
+     (define-key magit-status-mode-map (kbd "<up>") #'magit-section-backward)))
+
 ;; Not that frequently used
 (global-set-key (kbd "C-c r") #'my-counsel-recentf)
 
