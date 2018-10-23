737b407be6823195296267894aa2cb253d1285f7
whatacold <whatacold@gmail.com>
2018-10-23 23:09:13 +0800

    Enable `hideshow` for python.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -7,6 +7,7 @@
 
 (defun python-mode-hook-setup ()
   (unless (is-buffer-file-temp)
+    (hs-minor-mode)
     ;; http://emacs.stackexchange.com/questions/3322/python-auto-indent-problem/3338#3338
     ;; emacs 24.4 only
     (setq electric-indent-chars (delq ?: electric-indent-chars))))
