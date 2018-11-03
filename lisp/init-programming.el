8d55d4c39521116cf107dece888c0c5208e963a5
whatacold <whatacold@gmail.com>
2018-11-03 23:00:23 +0800

    Eglot ccls contact as a function.

--- lisp/init-programming.el
+++ lisp/init-programming.el
@@ -12,26 +12,28 @@
 ;; e.g. (setq whatacold/ccls-init-args '(:clang (:extraArgs ("-std=c++03"))))
 (defvar whatacold/ccls-init-args nil)
 
+(defun whatacold/eglot-ccls-contact (interactive-p)
+  "A contact function to assemble args for ccls."
+  (let ((json-object-type 'plist)
+        (json-array-type 'list)
+        result)
+    (push (format "-log-file=/tmp/ccls-%s.log"
+                  (file-name-base
+                   (directory-file-name
+                    (car
+                     (project-roots
+                      (project-current))))))
+          result)
+    (when whatacold/ccls-init-args
+      (push (format "-init=%s" (json-encode
+                                whatacold/ccls-init-args))
+            result))
+    (push "ccls" result)))
+
 (eval-after-load 'eglot
   '(progn
      (add-to-list 'eglot-server-programs
-                  (cons '(c-mode c++-mode foo-mode)
-                        #'(lambda (interactive-p)
-                            (let ((json-object-type 'plist)
-                                  (json-array-type 'list)
-                                  result)
-                              (push (format "-log-file=/tmp/ccls-%s.log"
-                                            (file-name-base
-                                             (directory-file-name
-                                              (car
-                                               (project-roots
-                                                (project-current))))))
-                                    result)
-                              (when whatacold/ccls-init-args
-                                (push (format "-init=%s" (json-encode
-                                                          whatacold/ccls-init-args))
-                                      result))
-                              (push "ccls" result)))))))
+                  (cons '(c-mode c++-mode foo-mode) #'whatacold/eglot-ccls-contact))))
 
 (setq avy-timeout-seconds 0.3)
 
